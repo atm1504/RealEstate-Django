8a71d9f9711b62b967062be6bddac4259e7810ac
Amartya Mondal <atm1504.in@gmail.com>
2021-01-16 17:01:58 +0530

    About index page done

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from listings.models import Listing
+from realtors.models import Realtor
 # Create your views here.
 
 def index(request):
@@ -11,4 +12,12 @@ def index(request):
     return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.order_by('-hire_date')
+    # GEt MVP
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context={
+        'realtors':realtors,
+        'mvp_realtors':mvp_realtors
+    }
+    return render(request, 'pages/about.html', context)
