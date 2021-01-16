f654d1041da0ca3d3947109504bd7bcdc5a20316
Amartya Mondal <atm1504.in@gmail.com>
2021-01-16 15:42:38 +0530

    Listing vieews

--- listings/views.py
+++ listings/views.py
@@ -5,10 +5,10 @@ from .models import Listing
 def index(request):
     listings= Listing.objects.all()
     context ={'name':"Atm", 'listings':listings}
-    
+
     return render(request, 'listings/listings.html',context)
 
-def listing(request):
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 def search(request):
