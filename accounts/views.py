5e2f7dc5736bccace9126748454702053a34fc05
Amartya Mondal <atm1504.in@gmail.com>
2021-01-16 20:33:06 +0530

    Fix issues

--- accounts/views.py
+++ accounts/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import redirect, render
 
 # Create your views here.
 def register(request):
-    return render(request,'accounts/register.html')
+    if request.method == 'POST':
+        print('Submitted Form')
+        return redirect('login')
+    else:
+        return render(request,'accounts/register.html')
 
 def login(request):
-    return render(request,'accounts/login.html')
+    if request.method == "POST":
+        print("Logged in requested")
+        return redirect('index')
+    else:
+        return render(request,'accounts/login.html')
 
 def logout(request):
     return redirect('index')
